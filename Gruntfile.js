91022c31b400daf640336c182e8bc45dcb442c20
volgunin <avolgunin@gmail.com>
2015-09-14 09:24:44 -0600

    autoFocus, compareTo, stopPropagation

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+'use strict'
+
+module.exports = function(grunt) {
+
+    require('load-grunt-tasks')(grunt)
+    grunt.loadNpmTasks('grunt-contrib-jshint')
+
+    grunt.initConfig({
+        concat: {
+            js: {
+                src: [
+                    'lib/index.js',
+                    'lib/directives/*.js',
+                    'lib/services/*.js',
+                    'lib/controllers/*.js'
+                ],
+                dest: './dist/angular-widgets.js'
+            }
+        },
+        uglify: {
+            js: {
+                src: ['./dist/angular-widgets.js'],
+                dest: './dist/angular-widgets.min.js',
+                options: {
+                    sourceMap: true
+                }
+            }
+        },
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc',
+                reporter: require('jshint-stylish')
+            },
+            all: ['Gruntfile.js', 'lib/**/*.js']
+        }
+    })
+
+    grunt.registerTask('default', [
+        'concat',
+        'uglify'
+    ])
+}
---
+++ dist/angular-widgets.js
@@ -0,0 +1,86 @@
+(function() {
+	'use strict'
+
+	angular.module('angular.widgets', [])
+})()
+/**
+ * @desc this directive sets a focus to input element
+ */
+
+(function() {
+	'use strict'
+
+	function autoFocus($timeout) {
+
+		function link(scope, element) {
+			$timeout(function() {
+				element[0].focus()
+			})
+		}
+
+		return {
+			restrict: 'A',
+			link: link
+		}
+	}
+
+	angular.module('angular.widgets')
+		.directive('autoFocus', ['$timeout', autoFocus])
+})()
+/**
+ * @desc this directive compares element's value with another element's value.
+ * 		it can be used to validate elements that must have identical values.
+ *		for instance, password and confirm password
+ */
+
+(function() {
+	'use strict'
+
+	function compareTo() {
+
+		function link(scope, element, attributes, ngModel) {
+			ngModel.$validators.compareTo = function(modelValue) {
+				return angular.isUndefined(modelValue) || modelValue.length === 0 || modelValue === scope.otherModelValue
+			}
+
+			scope.$watch('otherModelValue', function() {
+				ngModel.$validate()
+			})
+		}
+
+		return {
+			require: 'ngModel',
+			scope: {
+				otherModelValue: '=compareTo'
+			},
+			link: link
+		}
+	}
+
+	angular.module('angular.widgets')
+		.directive('compareTo', compareTo)
+})()
+/**
+ * @desc this directive stops propagation of click event
+ */
+
+(function() {
+	'use strict'
+
+	function stopPropagation() {
+
+		function link(scope, element) {
+			element.bind('click', function(e) {
+				e.stopPropagation()
+			})
+		}
+
+		return {
+			restrict: 'A',
+			link: link
+		}
+	}
+
+	angular.module('angular.widgets')
+		.directive('stopPropagation', stopPropagation)
+})()
