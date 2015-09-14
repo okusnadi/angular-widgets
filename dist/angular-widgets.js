7246f459ffb7b06b7382f63a408103d438d7a34d
volgunin <avolgunin@gmail.com>
2015-09-14 14:38:14 -0600

    add fileUpload and httpService

--- dist/angular-widgets.js
+++ dist/angular-widgets.js
@@ -71,6 +71,45 @@
 		.directive('compareTo', compareTo);
 })();
 /**
+ * @desc this directive upload a file
+ */
+
+(function() {
+	'use strict';
+
+	function fileUpload() {
+
+		function link(scope, element) {
+			var input = element.find('input');
+
+			element.bind('click', function(e) {
+				e.stopPropagation();
+				input.click();
+			});
+
+			element.bind('change', function(e) {
+				e.stopPropagation();
+				scope.fileUploadCallback({
+					files: input[0].files
+				});
+			});
+		}
+
+		return {
+			restrict: 'A',
+			transclude: true,
+			template: '<input type="file" style="position:absolute; opacity: 0"><ng-transclude></ng-transclude>',
+			scope: {
+				fileUploadCallback: '&fileUploadCallback'
+			},
+			link: link
+		};
+	}
+
+	angular.module('angular.widgets')
+		.directive('fileUpload', fileUpload);
+})();
+/**
  * @desc this directive stops propagation of click event
  */
 
@@ -93,6 +132,70 @@
 
 	angular.module('angular.widgets')
 		.directive('stopPropagation', stopPropagation);
+})();
+/**
+ * @desc this service provides basic wrapper around $http service
+ */
+
+(function() {
+	'use strict';
+
+	function httpService($http) {
+
+		function f(method, url, params, data, authenticate) {
+
+			params = params || {};
+			if ('GET' === method && angular.isUndefined(params.disableCache)) {
+				params.disableCache = (new Date()).getTime();
+			}
+			if (authenticate) {
+				params.authenticate = true;
+			}
+
+			return $http({
+				method: method,
+				params: params,
+				url: url,
+				data: data
+			});
+		}
+
+		function get(url, params) {
+			return f('GET', url, params, undefined, true);
+		}
+
+		function post(url, data, params) {
+			return f('POST', url, params, data, true);
+		}
+
+		function put(url, data) {
+			return f('PUT', url, undefined, data, true);
+		}
+
+		function remove(url, params) {
+			return f('DELETE', url, params);
+		}
+
+		function unauthGet(url, params) {
+			return f('GET', url, params);
+		}
+
+		function unauthPost(url, data, params) {
+			return f('POST', url, params, data);
+		}
+
+		return {
+			get: get,
+			post: post,
+			put: put,
+			remove: remove,
+			unauthGet: unauthGet,
+			unauthPost: unauthPost
+		};
+	}
+
+	angular.module('angular.widgets')
+		.factory('httpService', ['$http', httpService]);
 })();
 
 	return app;
